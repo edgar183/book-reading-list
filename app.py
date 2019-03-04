cd3e4d58f39f133f378a8379206216834769d4be
Edgars Peskaitis <edgars.peskaitis@gmail.com>
2019-03-04 13:09:05 +0000

    initial hello world index page seted up to test flask is working corectly

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://book-admin:boss123@localhost/bookLibrary'
+db = SQLAlchemy(app)
+
+meta = db.metadata
+engine = db.engine
+
+
 
 @app.route('/')
 def index():
@@ -10,5 +18,4 @@ def index():
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-debug=True)
+            port=int(os.environ.get('PORT')),debug=True)
