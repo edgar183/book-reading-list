4a13ad473962f8b019d9553b1c8c2bcbe68997b3
Edgars Peskaitis <edgars.peskaitis@gmail.com>
2019-03-07 14:11:11 +0000

    login form creted with validation

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, EqualTo
 
+#Registration form with validation rules.
 class RegisterForm(FlaskForm):
     name = StringField('First Name', validators=[DataRequired("Enter your first name.")])
     username = StringField('Username', validators=[DataRequired("User name must be from 2 to 20 charakters long."), Length(min=2, max=20)])
     password = StringField('Password', validators=[DataRequired()])
     confirm_password = StringField('Confirm Password', validators=[DataRequired(), EqualTo('Password')])
     submit = SubmitField('Sign Up')
+
+# Login from with remember me check box. Browswer will remember user in secure cookie for same time.
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired("User name must be from 2 to 20 charakters long."), Length(min=2, max=20)])
+    password = StringField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
     
