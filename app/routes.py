78667f8e935854cb8d6aac96e3738733b7fbce97
Edgars Peskaitis <edgars.peskaitis@gmail.com>
2019-03-07 14:36:09 +0000

    froms impoted and added to routes of ligin and register

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,20 @@
 from flask import render_template, url_for
 from app import models
 from app import app
+from forms import RegisterForm, LoginForm
 
 @app.route('/')
 def index():
     return render_template('index.html')
-    
-
-@app.route('/login')
-def login():
-    return render_template('login.html', title='Login')
-    
 
 @app.route('/register')
 def register():
-    return render_template('register.html', title='Register')
+    form = RegisterForm()
+    return render_template('register.html', title='Register', form=form)    
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+    
+
