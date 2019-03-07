a81658de05c1409088330b6a2673d5fba421d5e2
Edgars Peskaitis <edgars.peskaitis@gmail.com>
2019-03-07 16:30:53 +0000

    change varible manes of login_form and reg_form to form in all fields

--- app/routes.py
+++ app/routes.py
@@ -9,20 +9,20 @@ def index():
 
 @app.route('/register', methods=['GET','POST'])
 def register():
-    reg_form = RegisterForm()
-    if reg_form.validate_on_submit():
-        flash("Account created of user %s with username of %s!"%({reg_form.name.data}, {reg_form.username.data}), 'success')
+    form = RegisterForm()
+    if form.validate_on_submit():
+        flash("Account created of user %s with username of %s!"%({form.name.data}, {form.username.data}), 'success')
         return redirect(url_for('index'))
-    return render_template('register.html', title='Register', form=reg_form)    
+    return render_template('register.html', title='Register', form=form)    
 
 @app.route('/login', methods=['GET','POST'])
 def login():
-    login_form = LoginForm()
-    if login_form.validate_on_submit():
-        flash('%s You have been logged in!'%(login_form.username.data), 'success')
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('%s You have been logged in!'%(form.username.data), 'success')
         return redirect(url_for('index'))
     else:
         flash('Login Unsuccessful. Please check username and password', 'danger')
-    return render_template('login.html', title='Login', form=login_form)
+    return render_template('login.html', title='Login', form=form)
     
 
