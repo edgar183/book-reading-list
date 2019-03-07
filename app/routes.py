23fdd0dc9e159eed06393d1162348697d45bdcec
Edgars Peskaitis <edgars.peskaitis@gmail.com>
2019-03-07 15:53:55 +0000

    login validation if statment added with success and denger messages

--- app/routes.py
+++ app/routes.py
@@ -12,12 +12,17 @@ def register():
     reg_form = RegisterForm()
     if reg_form.validate_on_submit():
         flash("Account created of user %s with username of %s!"%({reg_form.name.data}, {reg_form.username.data}), 'success')
-        return redirect(url_for('home'))
+        return redirect(url_for('index'))
     return render_template('register.html', title='Register', form=reg_form)    
 
 @app.route('/login', methods=['GET','POST'])
 def login():
     login_form = LoginForm()
+    if login_form.validate_on_submit():
+        flash('%s You have been logged in!'%(login_form.username.data), 'success')
+        return redirect(url_for('index'))
+    else:
+        flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=login_form)
     
 
