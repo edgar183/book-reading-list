3a8cc12b3c0f085919f1f0c58308d01779da88b0
Edgars Peskaitis <edgars.peskaitis@gmail.com>
2019-03-07 15:04:47 +0000

    methods=['GET','POST'] added to register and login routes

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,20 @@
 from flask import render_template, url_for
 from app import models
 from app import app
-from forms import RegisterForm, LoginForm
+from app.forms import RegisterForm, LoginForm
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET','POST'])
 def register():
-    form = RegisterForm()
-    return render_template('register.html', title='Register', form=form)    
+    reg_form = RegisterForm()
+    return render_template('register.html', title='Register', form=reg_form)    
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
-    form = LoginForm()
-    return render_template('login.html', title='Login', form=form)
+    login_form = LoginForm()
+    return render_template('login.html', title='Login', form=login_form)
     
 
