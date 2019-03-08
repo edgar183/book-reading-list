fa9162acf7e85c2318810eb367d743287014310e
Edgars Peskaitis <edgars.peskaitis@gmail.com>
2019-03-08 18:03:04 +0000

    checking and changing some of the varible names to mach html template for regiater and login pages

--- app/forms.py
+++ app/forms.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired, Length, EqualTo
 #Registration form with validation rules.
 class RegisterForm(FlaskForm):
     name = StringField('First Name', validators=[DataRequired("Enter your first name.")])
-    username = StringField('Username', validators=[DataRequired("User name must be from 2 to 20 characters long."), Length(min=2, max=20)])
+    username = StringField('Username', validators=[DataRequired("User name must be from 2 to 20 charakters long."), Length(min=2, max=20)])
     password = StringField('Password', validators=[DataRequired("Passwords must be at least 8 characters long."),Length(min=3, max=8)])
     confirm_password = StringField('Confirm Password', validators=[DataRequired(), EqualTo('Password', message='Passwords must match')])
     submit = SubmitField('Sign Up')
