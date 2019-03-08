23105597aef4ccc82f7f29f8b50f39380606c46a
Edgars Peskaitis <edgars.peskaitis@gmail.com>
2019-03-08 12:13:14 +0000

    sqlAchamy URi changed to pint in local folder and use sqlit insted of mysql.

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 # init app
 app = Flask(__name__)
-SQLALCHEMY_DATABASE_URI = 'mysql://book-admin:boss123@localhost/bookLibrary'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///db/bookLibrary.db'
 # set up database location config
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://book-admin:boss123@localhost/bookLibrary'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/bookLibrary.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # init DB in app
