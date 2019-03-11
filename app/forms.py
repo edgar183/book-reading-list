b6d5ee0ad884df93075f6be40f8862e6a29c0ee2
Edgars Peskaitis <edgars.peskaitis@gmail.com>
2019-03-11 12:41:37 +0000

    validation messages removed

--- app/forms.py
+++ app/forms.py
@@ -4,16 +4,15 @@ from wtforms.validators import DataRequired, Length, EqualTo
 
 #Registration form with validation rules.
 class RegisterForm(FlaskForm):
-    name = StringField('First Name', validators=[DataRequired("Enter your first name.")])
-    username = StringField('Username', validators=[DataRequired("User name must be from 2 to 20 charakters long."), Length(min=2, max=20)])
-    password = PasswordField('Password', validators=[DataRequired("Passwords must be at least 8 characters long."),Length(min=3, max=8)])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('Password', message='Passwords must match')])
+    name = StringField('First Name', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField('Password', validators=[DataRequired(),Length(min=3, max=8)])
     submit = SubmitField('Sign Up')
 
 # Login from with remember me check box. Browswer will remember user in secure cookie for same time.
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired("Incorect username"), Length(min=2, max=20)])
-    password = PasswordField('Password', validators=[DataRequired("Incorect Password")])
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
     
