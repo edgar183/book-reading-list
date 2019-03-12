2d92c62fe0a54c95d974c7b46f23928a5dd69576
Edgars Peskaitis <edgars.peskaitis@gmail.com>
2019-03-12 17:10:14 +0000

    change of userName to username var

--- app/forms.py
+++ app/forms.py
@@ -5,13 +5,13 @@ from wtforms.validators import DataRequired, Length, EqualTo
 #Registration form with validation rules.
 class RegisterForm(FlaskForm):
     name = StringField('First Name', validators=[DataRequired()])
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    username = StringField('username', validators=[DataRequired(), Length(min=2, max=20)])
     password = PasswordField('Password', validators=[DataRequired(),Length(min=3, max=8)])
     submit = SubmitField('Sign Up')
 
 # Login from with remember me check box. Browswer will remember user in secure cookie for same time.
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    username = StringField('username', validators=[DataRequired(), Length(min=2, max=20)])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
