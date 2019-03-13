abc2f39cf99ab64b5e384b3c142e85f58debf445
Edgars Peskaitis <edgars.peskaitis@gmail.com>
2019-03-13 11:54:41 +0000

    adding password hashing function to the regidter method to encript password and add the new user to database

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, redirect
 from app import models
-from app import app
+from app import app, db, bcrypt
 from app.forms import RegisterForm, LoginForm
 
 @app.route('/')
@@ -11,8 +11,12 @@ def index():
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
-        flash("Account created of user %s with user name of %s!"%(form.name.data, form.username.data), 'success')
-        return redirect(url_for('index'))
+        hash_password = bcrypt.generate_password_hash(form.password.data)
+        user = models.User(name=form.name.data, username=form.username.data, password=hash_password)
+        db.session.add(user)
+        db.session.commit()
+        flash("%s your account has been created!"%(form.name.data), 'success')
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)    
 
 @app.route('/login', methods=['GET','POST'])
