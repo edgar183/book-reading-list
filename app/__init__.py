d71070a3e6c3fe52516aa9cb792b7e73b557657c
Edgars Peskaitis <edgars.peskaitis@gmail.com>
2019-03-13 11:47:49 +0000

    adding and initialaizing LoginManager

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 # init app
@@ -12,5 +14,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # init DB in app
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from app import routes
