480f4029b6e94ac05059da69cf9dc83a293ce52d
Edgars Peskaitis <edgars.peskaitis@gmail.com>
2019-03-13 16:31:17 +0000

    add update account form

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, ValidationError
 from app.models import User
@@ -22,4 +23,16 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+#Update account details form with validation rules.
+class UpdateAccountForm(FlaskForm):
+    name = StringField('First Name', validators=[DataRequired()])
+    username = StringField('username', validators=[DataRequired(), Length(min=2, max=20)])
+    submit = SubmitField('Update')
     
+    # checking the username alredy exist in the database and returning error message to the form
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('The user with this username exists. Please choos a different username.')
