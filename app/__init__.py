1128e6e74134ec3b8805b06bae7b88cdd18e8336
Edgars Peskaitis <edgars.peskaitis@gmail.com>
2019-03-13 20:41:28 +0000

    duplicate URI for SQLite removed

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,6 @@ from flask_login import LoginManager
 
 # init app
 app = Flask(__name__)
-SQLALCHEMY_DATABASE_URI = 'sqlite:///db/bookLibrary.db'
 # set up database location config
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/bookLibrary.db'
