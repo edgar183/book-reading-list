7219ec9b30c7747289c7f555cd1e838b18d188b5
Edgars Peskaitis <edgars.peskaitis@gmail.com>
2019-03-13 11:33:30 +0000

    validation rule if usrename alredy exists in database added

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
 from wtforms.validators import DataRequired, Length, EqualTo
+from app.models import User
 
 #Registration form with validation rules.
 class RegisterForm(FlaskForm):
@@ -8,7 +9,13 @@ class RegisterForm(FlaskForm):
     username = StringField('username', validators=[DataRequired(), Length(min=2, max=20)])
     password = PasswordField('Password', validators=[DataRequired(),Length(min=3, max=8)])
     submit = SubmitField('Sign Up')
-
+    
+    # checking the username alredy exist in the database and returning error message to the form
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).firat()
+        if user:
+            raise ValidationError('The user with this username exists. Please choos a different username.')
+            
 # Login from with remember me check box. Browswer will remember user in secure cookie for same time.
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[DataRequired(), Length(min=2, max=20)])
