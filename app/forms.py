642068f6c01d3b660cbe06ecb1618dcbf7403350
Edgars Peskaitis <edgars.peskaitis@gmail.com>
2019-03-13 11:52:47 +0000

    importing user table from models to handel user registration duplicates

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, ValidationError
 from app.models import User
 
 #Registration form with validation rules.
