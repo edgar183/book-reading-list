2b6eed4de3d4043f7ddae93dbdeba18e62c17895
Edgars Peskaitis <edgars.peskaitis@gmail.com>
2019-03-14 15:03:31 +0000

    data added to database

--- app/db.py
+++ app/db.py
@@ -1,3 +1,4 @@
+"""
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from app import SQLALCHEMY_DATABASE_URI
@@ -10,3 +11,4 @@ manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
+    """
