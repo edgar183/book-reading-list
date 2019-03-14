a5c3d19141b2fb0abeda77dc1214378cea4326e3
Edgars Peskaitis <edgars.peskaitis@gmail.com>
2019-03-14 11:18:58 +0000

    new model Author imported and new form creted to add new author to database

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, ValidationError
-from app.models import User
+from app.models import User, Author
 
 #Registration form with validation rules.
 class RegisterForm(FlaskForm):
@@ -41,7 +41,7 @@ class Add_Author(FlaskForm):
     full_name = StringField('Full Name', validators=[DataRequired()])
     submit = SubmitField('Add')
     
-    def validate_author(self, full_name):
+    def validate_full_name(self, full_name):
         author = Author.query.filter_by(full_name=full_name.data)
         if author:
-            raise ValidationError('The author with this full name alredy exists.')
+            raise ValidationError('The author with this name alredy exists.')
