1c7c7767dacd11b0a075dae2c582c6788b407940
Edgars Peskaitis <edgars.peskaitis@gmail.com>
2019-03-20 20:47:58 +0000

    all models imported from app to reduce typing models.<name>.somthing

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, url_for, flash, redirect, request
 from app import models
+from app.models import *
 from app import app, db, bcrypt
 from app.forms import RegisterForm, LoginForm, UpdateAccountForm, Add_Author, Add_Category, Add_Publisher, Add_Readinglist, Add_Book
 from flask_login import login_user, current_user, logout_user, login_required
@@ -8,12 +9,12 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route('/')
 @app.route('/index')
 def index():
-    authors = models.Author.query.all()
-    publishers = models.Publisher.query.all()
-    categories = models.Category.query.all()
-    users = models.User.query.all()
-    readinglists = models.Lists.query.all()
-    books = models.Book.query.all()
+    authors = Author.query.all()
+    publishers = Publisher.query.all()
+    categories = Category.query.all()
+    users = User.query.all()
+    readinglists = Lists.query.all()
+    books = Book.query.all()
     return render_template('index.html', authors=authors, publishers=publishers, categories=categories, users=users, readinglists=readinglists, books=books)
 
 # register account page
@@ -24,7 +25,7 @@ def register():
     form = RegisterForm()
     if form.validate_on_submit():
         hash_password = bcrypt.generate_password_hash(form.password.data)
-        user = models.User(name=form.name.data, username=form.username.data, password=hash_password)
+        user = User(name=form.name.data, username=form.username.data, password=hash_password)
         db.session.add(user)
         db.session.commit()
         flash("%s your account has been created!"%(form.name.data), 'success')
@@ -39,7 +40,7 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         # check user existst
-        user = models.User.query.filter_by(username=form.username.data).first()
+        user = User.query.filter_by(username=form.username.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             return redirect(url_for('index'))
@@ -58,7 +59,7 @@ def logout():
 @login_required
 def account():
     form = UpdateAccountForm()
-    readinglists = models.Lists.query.all()
+    readinglists = Lists.query.all()
     if form.validate_on_submit():
         current_user.name = form.name.data
         current_user.username = form.username.data
@@ -76,7 +77,7 @@ def account():
 def add_author():
     form = Add_Author()
     if form.validate_on_submit():
-        author = models.Author(full_name=form.full_name.data)
+        author = Author(full_name=form.full_name.data)
         db.session.add(author)
         db.session.commit()
         flash('New Author has been added!', 'success')
@@ -89,7 +90,7 @@ def add_author():
 def add_category():
     form = Add_Category()
     if form.validate_on_submit():
-        category = models.Category(Name=form.Name.data)
+        category = Category(Name=form.Name.data)
         db.session.add(category)
         db.session.commit()
         flash('New Category has been added!', 'success')
@@ -102,7 +103,7 @@ def add_category():
 def add_publisher():
     form = Add_Publisher()
     if form.validate_on_submit():
-        publisher = models.Publisher(Name=form.Name.data)
+        publisher = Publisher(Name=form.Name.data)
         db.session.add(publisher)
         db.session.commit()
         flash('New Publisher has been added!', 'success')
@@ -116,7 +117,7 @@ def add_readinglist():
     form = Add_Readinglist()
     if form.validate_on_submit():
         # check user existst
-        readinglist = models.Lists(ListName=form.ListName.data, user=current_user )
+        readinglist = Lists(ListName=form.ListName.data, user=current_user )
         db.session.add(readinglist)
         db.session.commit()
         flash('New reading list has been added!', 'success')
@@ -129,20 +130,44 @@ def add_readinglist():
 @login_required
 def add_book():
     form = Add_Book()
-    form.publisher.choices = [(publisher.PublisherId, publisher.Name) for publisher in models.Publisher.query.all()]
-    form.category.choices = [(category.CategoryId, category.Name) for category in models.Category.query.all()]
-    #publisher = models.Publisher.query.filter_by(PublisherId=form.publisher.data)
-    #category = models.Category.query.filter_by(CategoryId=form.category.data)
+    form.publisher.choices = [(publisher.PublisherId, publisher.Name) for publisher in Publisher.query.all()]
+    form.category.choices = [(category.CategoryId, category.Name) for category in Category.query.all()]
+    publisher = Publisher.query.filter_by(PublisherId=form.publisher.data)
+    category = Category.query.filter_by(CategoryId=form.category.data)
     if form.validate_on_submit():
-        book = models.Book(title=form.title.data, year=form.year.data, book_cover=form.book_cover.data, description=form.description.data, publisher_id=models.Publisher.query.filter_by(PublisherId=form.publisher.data), category_id=models.Category.query.filter_by(CategoryId=form.category.data))
-        db.session.add(book)
+        book = Book(title=form.title.data, year=form.year.data, book_cover=form.book_cover.data, description=form.description.data, publisher_id=publisher, category_id=category)
         db.session.commit()
         flash('New Book has been added!', 'success')
         return redirect(url_for('index'))
-    return render_template('add_book.html', title='New Book', form=form)
+    return render_template('add_book.html', title='New Book', form=form, legend='Add Book')
     
 # individual book page
 @app.route('/book/<int:book_isbn>')
 def book(book_isbn):
-    book = models.Book.query.get_or_404(book_isbn)
+    book = Book.query.get_or_404(book_isbn)
     return render_template('book.html', title=book.title, book=book)
+    
+# edit book information
+@app.route('/book/<int:book_isbn>/edit', methods=['GET','POST'])
+@login_required
+def edit_book(book_isbn):
+    book = Book.query.get_or_404(book_isbn)
+    form = Add_Book()
+    if form.validate_on_submit():
+        book.title = form.title.data
+        book.year = form.year.data
+        book.book_cover = form.book_cover.data
+        book.description = form.description.data
+        book.category_id = form.category.data
+        book.category_id = form.category.data
+        db.session.commit()
+        flash('The book detailes has been edited!', 'success')
+        return redirect(url_for('book', book_isbn=book.isbn))
+    elif request.method == 'GET':    
+        form.title.data = book.title
+        form.year.data = book.year
+        form.book_cover.data = book.book_cover
+        form.description.data = book.description
+        form.publisher.data = book.publisher
+        form.category.data = book.category
+    return render_template('add_book.html', title='Edit Book', form=form, legend='Edit Book')
