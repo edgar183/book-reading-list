865eb762aa654cf198911c95f3c46420f7d71cfb
Edgars Peskaitis <edgars.peskaitis@gmail.com>
2019-03-26 11:42:04 +0000

    remove sql Alchemy config of echo the sql querys in terminal

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 # set up database location config
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/bookLibrary.db'
-app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # init DB in app
