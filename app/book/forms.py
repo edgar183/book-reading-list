514dbff883cfc65343ae3392e565aa3614d46e66
Edgars Peskaitis <edgars.peskaitis@gmail.com>
2019-04-09 12:42:01 +0000

    adding querySelectField option in form to edit book, NOT WORKING

--- app/book/forms.py
+++ app/book/forms.py
@@ -1,7 +1,17 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, SelectField
 from wtforms.validators import DataRequired
-from app.models import Book
+from app.models import Book, Publisher, Category, Author
+from wtforms_sqlalchemy.fields import QuerySelectField
+
+def choice_publisher():
+    return Publisher.query.all()
+    
+def choice_category():
+    return Category.query.all()
+    
+def choice_author():
+    return Author.query.all()
 
 class Add_Book(FlaskForm):
     title = StringField('Book Title', validators=[DataRequired()])
@@ -9,8 +19,11 @@ class Add_Book(FlaskForm):
     book_cover = StringField('URL link for book cover picture', validators=[DataRequired()])
     description = TextAreaField('Book description ', validators=[DataRequired()])
     publisher = SelectField('Publisher', coerce=int)
+    #publisher = QuerySelectField(query_factory=choice_publisher, allow_blank=True,get_label='Name')
     category = SelectField('Category', coerce=int)
+    #category = QuerySelectField(query_factory=choice_category, allow_blank=True,get_label='Name')
     author = SelectField('Author', coerce=int)
+    #author = QuerySelectField(query_factory=choice_author, allow_blank=True,get_label='full_name')
     submit = SubmitField('Add')
     
 
