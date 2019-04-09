6b63eb163d403bd8aa31ec30a53aa1723aeae98f
Edgars Peskaitis <edgars.peskaitis@gmail.com>
2019-04-09 14:29:03 +0000

    using QuerySelectField insted of SelectField in form for better database calls

--- app/book/forms.py
+++ app/book/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField, SelectField
+from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
-from app.models import Book, Publisher, Category, Author
-from wtforms_sqlalchemy.fields import QuerySelectField
+from app.models import Book, Publisher, Category, Author, Lists
+from wtforms_alchemy.fields import QuerySelectField
 
 def choice_publisher():
     return Publisher.query.all()
@@ -18,16 +18,14 @@ class Add_Book(FlaskForm):
     year = StringField('Year Published', validators=[DataRequired()])
     book_cover = StringField('URL link for book cover picture', validators=[DataRequired()])
     description = TextAreaField('Book description ', validators=[DataRequired()])
-    publisher = SelectField('Publisher', coerce=int)
-    #publisher = QuerySelectField(query_factory=choice_publisher, allow_blank=True,get_label='Name')
-    category = SelectField('Category', coerce=int)
-    #category = QuerySelectField(query_factory=choice_category, allow_blank=True,get_label='Name')
-    author = SelectField('Author', coerce=int)
-    #author = QuerySelectField(query_factory=choice_author, allow_blank=True,get_label='full_name')
+    publisher = QuerySelectField(query_factory=choice_publisher, allow_blank=True,get_label='Name')
+    category = QuerySelectField(query_factory=choice_category, allow_blank=True,get_label='Name')
+    author = QuerySelectField(query_factory=choice_author, allow_blank=True,get_label='full_name')
     submit = SubmitField('Add')
     
-
+def choice_lists():
+    return Lists.query.all()
 
 class Add_book_to_readinglit(FlaskForm):
-    lists = SelectField('Reading Lists', coerce=int)
+    lists = QuerySelectField(query_factory=choice_lists, allow_blank=True,get_label='ListName')
     submit = SubmitField('Add')
