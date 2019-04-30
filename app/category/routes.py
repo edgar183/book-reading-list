0a4bdfa935ceb5ab5598cf0300b822e5c9f03225
Edgars Peskaitis <edgars.peskaitis@gmail.com>
2019-04-30 14:30:52 +0000

    edit category routes to render templates with all components, register and login froms included

--- app/category/routes.py
+++ app/category/routes.py
@@ -20,20 +20,22 @@ def all_categories():
     form_login = LoginForm() 
     form_register = RegisterForm()
     categories = Category.query.all()
-    return render_template('category/categories.html', all_categories=categories, title='Categories', form_cat=form_cat, form_login=form_login, form_register=form_register)
+    return render_template('category/categories.html', categories=categories, title='Categories', form_cat=form_cat, form_login=form_login, form_register=form_register)
 
 # add category     
 @categories.route('/categories', methods=['GET','POST'])
 @login_required
 def add_category():
     form_cat = Add_Category()
+    form_login = LoginForm()
+    form_register = RegisterForm()
     if form_cat.validate_on_submit():
-        category = Category(Name=form.Name.data)
+        category = Category(Name=form_cat.Name.data)
         db.session.add(category)
         db.session.commit()
         flash('New Category has been added!', 'success')
-        return redirect(url_for('categories.all_categories'))
-    return render_template('category/categories.html', title='Categories ', form_cat=form_cat)
+    categories = Category.query.all()
+    return render_template('category/categories.html', categories=categories, title='Categories', form_cat=form_cat, form_login=form_login, form_register=form_register)
 
 # edit category name
 @categories.route('/categories/<int:category_id>/edit', methods=['GET','POST'])
@@ -42,12 +44,12 @@ def edit_category(category_id):
     category = Category.query.get_or_404(category_id)
     form_cat = Add_Category()
     if form_cat.validate_on_submit():
-        category.Name = form.Name.data
+        category.Name = form_cat.Name.data
         db.session.commit()
         flash('The category name has been updated!', 'success')
         return redirect(url_for('categories.all_categories', category_id=category.CategoryId))
     elif request.method == 'GET': 
-        form.Name.data = category.Name
+        form_cat.Name.data = category.Name
     return render_template('category/add_category.html', title='Edit Category ', form_cat=form_cat, legend='Edit Category Name')
 
 # delete category from database
