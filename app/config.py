69a7f0449442204eb19809d142c208371b12706c
Edgars Peskaitis <edgars.peskaitis@gmail.com>
2019-05-15 12:59:07 +0000

    changing sekret key to random number

--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
+import os
 class Config:
     # set up database location config
-    SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
+    SECRET_KEY=os.urandom(24)
     SQLALCHEMY_DATABASE_URI = 'sqlite:///db/bookLibrary.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
